e1fa3c47ead4afdbd217180586cf1ab197f25a0f
Ponga Villacorta <dpongavillacorta@gmail.com>
2022-10-21 12:06:41 +0200

    Melendi

--- Prueba.java
+++ Prueba.java
@@ -3,9 +3,11 @@ public class Prueba {
 	public static void main(String args[]) {
 
 
-	System.out.println("Ponga Villacorta");
+	
 	System.out.println ("21 años naci el 3-7-2001");
 	System. out.println("Pizza");
+	System.out.println( "Melendi");
+
 	}
 
 }
